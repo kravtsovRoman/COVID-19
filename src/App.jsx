d6f89c124f21cf6962bf4eaf4d658096c77f1c75
Roman <rozario91208@gmail.com>
2020-04-19 19:05:49 +0300

    create Card component, styled and fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.scss';
+import { fetchData } from './api';
+
 class App extends React.Component {
+
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const FetchedData = await fetchData();
+
+    this.setState({
+      data: FetchedData
+    })
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
